8641ffcac1ba5893ee4b699cdf6918a0531c797c
ayluu-06 <pintosaylin7@gmail.com>
2025-02-01 19:50:53 -0300

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+class that defines a square
+"""
+
+
+class Square:
+    """
+    space to start defining the square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+
+        return self.__size ** 2
+    pass
