10c237759ef49bb2d2abf0fbc5bcef0ac854fbfb
ayluu-06 <pintosaylin7@gmail.com>
2025-02-06 00:39:21 -0300

    exactly an instance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+modulo que contiene la clase(?
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of the specified class
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
