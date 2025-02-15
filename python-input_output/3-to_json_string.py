512242b6eb4ee038275c67de605710a4bd057151
ayluu-06 <pintosaylin7@gmail.com>
2025-02-14 22:43:21 -0300

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""
+module documented task 3
+"""
+
+
+def to_json_string(my_obj):
+    """
+    function documented task 3
+    """
+    return json.dumps(my_obj)
