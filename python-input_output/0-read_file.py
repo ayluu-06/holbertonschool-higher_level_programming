6d00d5e3294b514c5dc5fa62792d9d4c4834a4e9
ayluu-06 <pintosaylin7@gmail.com>
2025-02-14 22:01:25 -0300

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+module decumented task 0
+"""
+
 
 def read_file(filename=""):
+    """
+    function documented task 0
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
