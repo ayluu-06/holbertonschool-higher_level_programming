941fe67d5a2d2c55bd01bf09ad214598e82986fb
ayluu-06 <pintosaylin7@gmail.com>
2025-02-15 15:35:55 -0300

    From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pytohn3
+
+"""
+module documented task 4
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    function documented task 4
+    """
+    return json.loads(my_str)
