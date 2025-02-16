e7b8bbab5ac71879ddce6da0e613e656d4667e3b
ayluu-06 <pintosaylin7@gmail.com>
2025-02-15 21:17:56 -0300

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+module documented task 9
+"""
+
+
+class Student:
+    """
+    class documented task 9
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for
+                    key in attrs if hasattr(self, key)}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
