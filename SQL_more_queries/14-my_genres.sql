255abacbf07bdee5fa5fee02b1e2b840ec94704c
ayluu-06 <pintosaylin7@gmail.com>
2025-03-29 23:54:21 -0300

    My genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,7 @@
---'hbtn_0d_tvshows' to list all genres of show 'Dexter'
+-- lists all genres of the show Dexter
 SELECT tv_genres.name
-FROM tv_shows 
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
